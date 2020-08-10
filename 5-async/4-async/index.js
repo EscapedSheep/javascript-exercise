6bcfc2161409cc68113187a47fd36b3bf8d61535
EscapedSheep <sfgmdf001@126.com>
2020-08-10 19:33:06 +0800

    [maida] completed all task 5

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,13 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    document.writeln(result.name);
+  } catch (e) {
+    console.log(e);
+  }
   // end -->
 }
 
