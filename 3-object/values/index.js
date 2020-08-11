dd4b640c0b63e53138a03efc7cb9d08a72c543c8
EscapedSheep <sfgmdf001@126.com>
2020-08-11 09:38:35 +0800

    [maida] fix lint problem

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,7 +1,7 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
   return Object.values(source)
-    .map(value => parseInt(value, 10))
+    .map(value => Number(value))
     .reduce((previousValue, currentValue) => {
       return previousValue + currentValue;
     });
